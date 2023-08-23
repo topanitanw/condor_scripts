d5b55cf982f7ae017c070f1d4cc96e6a0375d0fb
topanitanw <sassofono@hotmail.com>
2023-08-23 16:38:29 -0500

    add the submitted_job_status.py to replace the condor-mq alias

--- makefile
+++ makefile
@@ -1,3 +1,16 @@
+PYTHON=$(shell which python)
+
+avail:
+	$(PYTHON) scripts/bin/server_availability.py
+.PHONY: avail
+
+status:
+	$(PYTHON) scripts/bin/submitted_job_status.py
+.PHONY: status
+
+requirements.txt:
+	pipreqs . --force
+
 format:
 	yapf --in-place *.py;
 .PHONY: format
---
+++ scripts/bin/submitted_job_status.py
@@ -0,0 +1,47 @@
+import htcondor
+import pandas as pd
+
+status_string = {}
+for member in htcondor.JobStatus:
+    status_string[member.value] = member.name
+
+# Connect to the HTCondor collector
+coll = htcondor.Collector()
+
+# Get a list of jobs from the collector
+schedd_ads = coll.locate(htcondor.DaemonTypes.Schedd)
+schedd = htcondor.Schedd(schedd_ads)
+jobs = schedd.query()
+
+# Create a list to store job data
+job_data = []
+
+# Collect job status data
+for job in jobs:
+    print(job)
+    cluster_id = job.get("ClusterId")
+    proc_id = job.get("ProcId")
+    status = job.get("JobStatus")
+    owner = job.get("Owner")
+    remote_host = job.get("RemoteHost", "None").split(".")[0]
+
+    if cluster_id is not None and proc_id is not None and status is not None:
+        print(cluster_id, proc_id, status)
+        status_str = status_string[status]
+        job_data.append(
+            {
+                "ClusterId": cluster_id,
+                "ProcId": proc_id,
+                "Status": status_str,
+                "Owner": owner,
+                "RemoteHost": remote_host,
+            }
+        )
+    else:
+        print("Invalid job data:", job)
+
+# Create a DataFrame
+df = pd.DataFrame(job_data)
+
+# Print the DataFrame to stdout
+print(df)
